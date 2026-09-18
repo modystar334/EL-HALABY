5faf4ff63bf57cacddcd72cb18b46a3172430bd2
modystar334 <63385273+modystar334@users.noreply.github.com>
2026-09-18 10:02:02 +0200

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,3 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = (req, res) => {
-  res.status(200).json([]);
+  const filePath = path.join(process.cwd(), "data", "products.json");
+  const products = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+  res.status(200).json(products);
 };
